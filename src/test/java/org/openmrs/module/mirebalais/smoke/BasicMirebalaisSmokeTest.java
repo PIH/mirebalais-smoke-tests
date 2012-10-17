29d6cd7020e4354c67aa6956bd831e766336faed
Émerson Hernandez <emerson.hernandez@gmail.com>
2012-10-17 18:29:10 -0300

    Some refactoring to reuse driver/wait. Also, some new code to test phonetics

---
+++ src/test/java/org/openmrs/module/mirebalais/smoke/BasicMirebalaisSmokeTest.java
@@ -0,0 +1,33 @@
+package org.openmrs.module.mirebalais.smoke;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasicMirebalaisSmokeTest {
+
+	protected static WebDriver driver;
+    protected static Wait<WebDriver> wait;
+    
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver","chromedriver");
+    	driver = new ChromeDriver();
+    	
+    	wait = new WebDriverWait(driver, 30);
+		driver.get("http://bamboo.pih-emr.org:8080/mirebalais");
+		
+		specificSetUp();
+	}
+
+	protected abstract void specificSetUp();
+	
+	@After
+    public void tearDown() {
+    	//driver.close();
+    }
+	
+}
