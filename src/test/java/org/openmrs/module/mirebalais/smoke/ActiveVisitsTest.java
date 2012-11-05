023f83e303016c185086891dabbef87cf65f8101
djazayeri <jazayeri@alum.mit.edu>
2012-11-04 16:01:42 -0800

    License

--- src/test/java/org/openmrs/module/mirebalais/smoke/ActiveVisitsTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/ActiveVisitsTest.java
@@ -1,7 +1,18 @@
-package org.openmrs.module.mirebalais.smoke;
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+package org.openmrs.module.mirebalais.smoke;
 
 import org.junit.Test;
 import org.openmrs.module.mirebalais.smoke.pageobjects.AppDashboard;
@@ -11,6 +22,8 @@ import org.openmrs.module.mirebalais.smoke.pageobjects.LoginPage;
 import org.openmrs.module.mirebalais.smoke.pageobjects.PatientDashboard;
 import org.openmrs.module.mirebalais.smoke.pageobjects.Registration;
 import org.openqa.selenium.By;
+
+import static org.junit.Assert.assertTrue;
 
 public class ActiveVisitsTest extends BasicMirebalaisSmokeTest{
 
