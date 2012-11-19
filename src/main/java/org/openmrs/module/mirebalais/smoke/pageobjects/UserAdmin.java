9bc816167125b7ada514099641384edf559b740a
djazayeri <jazayeri@alum.mit.edu>
2012-11-18 21:53:38 -0800

    Fixing one smoke test

--- src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/UserAdmin.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/UserAdmin.java
@@ -1,3 +1,17 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
 package org.openmrs.module.mirebalais.smoke.pageobjects;
 
 import java.util.List;
@@ -14,29 +28,30 @@ public class UserAdmin extends AbstractPageObject {
 
 
 	public void createAccount(String firstName, String lastName, String username, String password) {
-		driver.findElement(By.linkText("Create Account")).click();
-		driver.findElement(By.name("givenName")).sendKeys(firstName);
-		driver.findElement(By.name("familyName")).sendKeys(lastName);
-		
-		// clicar em user account details
-		driver.findElement(By.xpath("/html/body/div/div[2]/form/fieldset[2]/div[2]/input")).click();
-		
-		driver.findElement(By.name("username")).sendKeys(username);
-		driver.findElement(By.name("password")).sendKeys(password);
-		driver.findElement(By.name("confirmPassword")).sendKeys(password);
+        driver.findElement(By.linkText("Manage Accounts")).click();
+        driver.findElement(By.id("create-account-button")).click();
+        driver.findElement(By.name("givenName")).sendKeys(firstName);
+        driver.findElement(By.name("familyName")).sendKeys(lastName);
 
-		WebElement select = driver.findElement(By.name("privilegeLevel"));
-	    List<WebElement> options = select.findElements(By.tagName("option"));
-	    for (WebElement option : options) {
-	        if("Privilege Level: Full".equals(option.getText()))
-	            option.click();
-	    }
-	    
-	    driver.findElement(By.xpath("//*[@id='capabilities']")).click();
+        // clicar em user account details
+        driver.findElement(By.id("createUserAccountButton")).click();
 
-	    // clicar em save button
-	    driver.findElement(By.xpath("/html/body/div/div[2]/form/input[3]")).click();
-	}
+        driver.findElement(By.name("username")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("confirmPassword")).sendKeys(password);
+
+        WebElement select = driver.findElement(By.name("privilegeLevel"));
+        List<WebElement> options = select.findElements(By.tagName("option"));
+        for (WebElement option : options) {
+            if ("Privilege Level: Full".equals(option.getText()))
+                option.click();
+        }
+
+        driver.findElement(By.xpath("//*[@id='capabilities']")).click();
+
+        // clicar em save button
+        driver.findElement(By.id("save-button")).click();
+    }
 
 	public boolean isAccountCreatedSuccesfully() {
 		return driver.findElement(By.id("info-message")).getText().contains("Saved account successfully");
