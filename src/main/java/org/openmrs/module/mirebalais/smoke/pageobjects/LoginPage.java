a3d0d003922d211153695553549c591403eb48dc
Émerson Hernandez <emerson.hernandez@gmail.com>
2013-03-06 18:26:20 -0300

    Trying to handle bamboo's problem with multiple deploys.

--- src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/LoginPage.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/LoginPage.java
@@ -2,6 +2,9 @@ package org.openmrs.module.mirebalais.smoke.pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 	
@@ -12,12 +15,24 @@ public class LoginPage {
 	}
 	
 	public void logIn(String user, String password) {
+		dealingWithConcurrentDeploys();
+		
 		driver.findElement(By.id("username")).sendKeys(user);
     	driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.cssSelector("#sessionLocation li")).click();
     	driver.findElement(By.id("login-button")).click();
 	}
 	
+	private void dealingWithConcurrentDeploys() {
+		Wait<WebDriver> wait = new WebDriverWait(driver, 300);
+		wait.until(new ExpectedCondition<Boolean>() {
+			@Override
+			public Boolean apply(WebDriver webDriver) {
+				return webDriver.findElement(By.id("username")).isDisplayed();
+			}
+		});
+	}
+
 	public void logInAsAdmin() {
 		this.logIn("admin", "Admin123");
 	}
