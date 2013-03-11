ea0202d777f329c878c3c287848ff36ba6b53f66
mogoodrich <mgoodrich@pih.org>
2013-03-11 10:28:16 -0400

    fix for single x-ray test

--- src/test/java/org/openmrs/module/mirebalais/smoke/OrdersTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/OrdersTest.java
@@ -1,38 +1,67 @@
 package org.openmrs.module.mirebalais.smoke;
 
 import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.module.mirebalais.smoke.pageobjects.AppDashboard;
+import org.openmrs.module.mirebalais.smoke.pageobjects.CheckIn;
 import org.openmrs.module.mirebalais.smoke.pageobjects.LoginPage;
 import org.openmrs.module.mirebalais.smoke.pageobjects.PatientDashboard;
+import org.openmrs.module.mirebalais.smoke.pageobjects.PatientRegistrationDashboard;
+import org.openmrs.module.mirebalais.smoke.pageobjects.Registration;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class OrdersTest extends BasicMirebalaisSmokeTest {
 
-	private LoginPage loginPage;
-    private AppDashboard appDashboard;
+    private CheckIn checkIn;
+    private static LoginPage loginPage;
+    private Registration registration;
+    private PatientRegistrationDashboard patientRegistrationDashboard;
     private PatientDashboard patientDashboard;
+    private AppDashboard appDashboard;
+    private String patientIdentifier;
     
     private static final String STUDY_1 = "Chest, 1 view (X-ray)";
     private static final String STUDY_2 = "Elbow - Left, 2 views (X-ray)";
     
     @Before
 	public void setUp() {
-		loginPage = new LoginPage(driver);
-		appDashboard = new AppDashboard(driver);
-		patientDashboard = new PatientDashboard(driver);
+        loginPage = new LoginPage(driver);
+        registration = new Registration(driver);
+        patientDashboard = new PatientDashboard(driver);
+        patientRegistrationDashboard = new PatientRegistrationDashboard(driver);
+        checkIn = new CheckIn(driver);
+        appDashboard = new AppDashboard(driver);
 	}
     
 	@Test
 	public void orderSingleXRay() {
-		loginPage.logInAsAdmin();
-		appDashboard.openActiveVisitsApp();
-		
-		//TODO: create an user to do this instead of using this from other test
-		driver.findElement(By.linkText("UNKNOWN UNKNOWN")).click();
+
+        loginPage.logInAsAdmin();
+        appDashboard.openPatientRegistrationApp();
+        registration.goThruRegistrationProcessWithoutPrintingCard();
+        patientIdentifier = patientRegistrationDashboard.getIdentifier();
+
+        appDashboard.findPatientById(patientIdentifier);
+        patientDashboard.startVisit();
+
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, 5);
+        wait.until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver webDriver) {
+                return webDriver.findElement(By.cssSelector("div.visit-status")).isDisplayed();
+            }
+        });
+        assertTrue(patientDashboard.hasActiveVisit());
 		patientDashboard.orderXRay(STUDY_1, STUDY_2);
 		
 		assertThat(patientDashboard.countEncouters(PatientDashboard.RADIOLOGY), not(0));
