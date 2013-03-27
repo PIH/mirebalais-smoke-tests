1ca2396e0e72c32f9f7a2aa0fce07f187869114f
Émerson Hernandez <emerson.hernandez@gmail.com>
2013-03-27 10:14:28 -0300

    Ignoring archives room test. Printer is not working on bamboo.

--- src/test/java/org/openmrs/module/mirebalais/smoke/ArchivesRoomFlowTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/ArchivesRoomFlowTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openmrs.module.mirebalais.smoke.pageobjects.AppDashboard;
 import org.openmrs.module.mirebalais.smoke.pageobjects.ArchivesRoomApp;
@@ -40,7 +41,7 @@ public class ArchivesRoomFlowTest extends BasicMirebalaisSmokeTest {
 		archivesRoomApp = new ArchivesRoomApp(driver);
 	}
 
-	
+	@Ignore // Printer is not working on bamboo
 	@Test
 	public void requestRecord() {
 		String dossieNumber = null;
