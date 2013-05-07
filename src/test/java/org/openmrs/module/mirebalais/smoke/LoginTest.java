10905aed684ecdddb2846f829d90a1ea17f4dd74
Émerson Hernandez <emerson.hernandez@gmail.com>
2013-05-07 16:42:14 -0300

    Changing the wait condition to be the assertion in the change location test

--- src/test/java/org/openmrs/module/mirebalais/smoke/LoginTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/LoginTest.java
@@ -1,13 +1,9 @@
 package org.openmrs.module.mirebalais.smoke;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.module.mirebalais.smoke.pageobjects.HeaderPage;
 import org.openmrs.module.mirebalais.smoke.pageobjects.LoginPage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Wait;
@@ -31,14 +27,13 @@ public class LoginTest extends BasicMirebalaisSmokeTest {
 		loginPage.logInAsAdmin();
 		headerPage.changeLocation(NEW_LOCATION);
 		
-		Wait<WebDriver> wait = new WebDriverWait(driver, 2);
+		Wait<WebDriver> wait = new WebDriverWait(driver, 10);
     	wait.until(new ExpectedCondition<Boolean>() {
 			@Override
 			public Boolean apply(WebDriver webDriver) {
-				return webDriver.findElement(By.className("icon-caret-down")).isDisplayed();
+				return headerPage.getLocation().contains(NEW_LOCATION);
 			}
 		});
-		assertThat(headerPage.getLocation().contains(NEW_LOCATION), is(true));
     }
 	
 }
