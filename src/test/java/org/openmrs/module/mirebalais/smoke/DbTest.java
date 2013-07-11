33af6f27ee64b6e5d60e1322cf7670cb35f53f31
César Henrique Vortmann <cvortmann@gmail.com>
2013-07-11 19:04:53 -0300

    #700 - getting a new valid patient identifier for every test.

--- src/test/java/org/openmrs/module/mirebalais/smoke/DbTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/DbTest.java
@@ -21,12 +21,16 @@ import org.openmrs.module.mirebalais.smoke.helper.SmokeTestProperties;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public abstract class DbTest extends BasicMirebalaisSmokeTest {
 
     private static SmokeTestProperties properties = new SmokeTestProperties();
     private static JdbcDatabaseTester tester;
     private IDataSet dataset;
+    private String patientIdentifierValue;
 
     @BeforeClass
     public static void setDatabaseConnection() throws ClassNotFoundException {
@@ -42,8 +46,11 @@ public abstract class DbTest extends BasicMirebalaisSmokeTest {
     @Before
     public void setUp() throws Exception {
         try {
-            testPatient = new Patient("Y00001", "Crash Test Dummy", getNextAutoIncrementFor("person"), getPatientIdentifierId(),
+            testPatient = new Patient(getNextValidPatientIdentifier(), "Crash Test Dummy", getNextAutoIncrementFor("person"), getPatientIdentifierId(),
                     getNextAutoIncrementFor("person_name"), getNextAutoIncrementFor("person_address"), getNextAutoIncrementFor("patient_identifier"));
+
+            lockPatientIdentifier();
+
             dataset = createDataset();
 
             DatabaseOperation.INSERT.execute(getConnection(), dataset);
@@ -62,16 +69,18 @@ public abstract class DbTest extends BasicMirebalaisSmokeTest {
 
             createdData = new QueryDataSet(getConnection());
             createdData.addTable("name_phonetics", "select * from name_phonetics where person_name_id = " + testPatient.getPerson_name_id());
-            createdData.addTable("patient_identifier", "select * from patient_identifier where patient_id="+ testPatient.getId());
-            createdData.addTable("emr_paper_record_request", "select * from emr_paper_record_request where patient_id="+ testPatient.getId());
+            createdData.addTable("patient_identifier", "select * from patient_identifier where patient_id=" + testPatient.getId());
+            createdData.addTable("emr_paper_record_request", "select * from emr_paper_record_request where patient_id=" + testPatient.getId());
             createdData.addTable("visit", "select * from visit where patient_id=" + testPatient.getId());
-            createdData.addTable("encounter", "select * from encounter where patient_id="+ testPatient.getId());
-            createdData.addTable("obs", "select * from obs where encounter_id in (select encounter_id from encounter where patient_id="+ testPatient.getId() + ")");
-            createdData.addTable("encounter_provider", "select * from encounter_provider where encounter_id in (select encounter_id from encounter where patient_id="+ testPatient.getId() + ")");
+            createdData.addTable("encounter", "select * from encounter where patient_id=" + testPatient.getId());
+            createdData.addTable("obs", "select * from obs where encounter_id in (select encounter_id from encounter where patient_id=" + testPatient.getId() + ")");
+            createdData.addTable("encounter_provider", "select * from encounter_provider where encounter_id in (select encounter_id from encounter where patient_id=" + testPatient.getId() + ")");
 
             DatabaseOperation.DELETE.execute(getConnection(), createdData);
 
             DatabaseOperation.DELETE.execute(getConnection(), dataset);
+
+            unlockPatientIdentifier();
         } catch (Exception e) {
             e.printStackTrace();
             throw new Exception("tear down failed", e);
@@ -87,6 +96,28 @@ public abstract class DbTest extends BasicMirebalaisSmokeTest {
         Template template = handlebars.compile("datasets/patients_dataset.xml");
 
         return new FlatXmlDataSetBuilder().build(new InputStreamReader(IOUtils.toInputStream(template.apply(testPatient))));
+    }
+
+    private String getNextValidPatientIdentifier() throws Exception {
+        ITable patientIdentifier = getConnection().createQueryTable("idgen_pooled_identifier",
+                "select * from idgen_pooled_identifier where date_used is null limit 1");
+
+        patientIdentifierValue = (String) patientIdentifier.getValue(0, "identifier");
+
+        return patientIdentifierValue;
+    }
+
+    private void lockPatientIdentifier() throws SQLException {
+        setDateUsedOfPatientIdentifierTo("sysdate()");
+    }
+
+    private void unlockPatientIdentifier() throws SQLException {
+        setDateUsedOfPatientIdentifierTo("null");
+    }
+
+    private void setDateUsedOfPatientIdentifierTo(String date) throws SQLException {
+        Connection connection = DriverManager.getConnection(properties.getDatabaseUrl(), properties.getDatabaseUsername(), properties.getDatabasePassword());
+        connection.createStatement().executeUpdate("update idgen_pooled_identifier set date_used = " + date + " where identifier = '" + patientIdentifierValue + "'");
     }
 
     private Integer getPatientIdentifierId() throws Exception {
