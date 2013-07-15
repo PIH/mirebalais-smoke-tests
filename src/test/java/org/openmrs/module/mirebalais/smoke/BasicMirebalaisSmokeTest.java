a43d772774296002277375f8b88c488acb7c0b7b
Fernando Freire <thenano@gmail.com>
2013-07-15 16:17:04 -0300

    #700 - adding a screeshot capture for failed tests

--- src/test/java/org/openmrs/module/mirebalais/smoke/BasicMirebalaisSmokeTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/BasicMirebalaisSmokeTest.java
@@ -1,7 +1,12 @@
 package org.openmrs.module.mirebalais.smoke;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.openmrs.module.mirebalais.smoke.dataModel.Patient;
 import org.openmrs.module.mirebalais.smoke.helper.SmokeTestDriver;
 import org.openmrs.module.mirebalais.smoke.pageobjects.AppDashboard;
@@ -9,11 +14,28 @@ import org.openmrs.module.mirebalais.smoke.pageobjects.LoginPage;
 import org.openmrs.module.mirebalais.smoke.pageobjects.PatientDashboard;
 import org.openmrs.module.mirebalais.smoke.pageobjects.PatientRegistrationDashboard;
 import org.openmrs.module.mirebalais.smoke.pageobjects.Registration;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import java.io.File;
+import java.io.IOException;
+
 public abstract class BasicMirebalaisSmokeTest {
-	
-	protected static LoginPage loginPage;
+
+    @Rule
+    public TestRule testWatcher = new TestWatcher() {
+        @Override
+        public void failed(Throwable t, Description test) {
+            File tempFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            try {
+                FileUtils.copyFile(tempFile, new File("screenshots/" + test.getDisplayName() + ".png"));
+            } catch (IOException e) {
+            }
+        }
+    };
+
+    protected static LoginPage loginPage;
 	
 	protected static WebDriver driver;
 	
