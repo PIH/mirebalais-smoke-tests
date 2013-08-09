2216980f7e8be3856d06b5dbad5b84cc067f7871
César Henrique Vortmann <cvortmann@gmail.com>
2013-08-09 14:57:48 -0300

    #744 - edit transfer within hospital test.

--- src/test/java/org/openmrs/module/mirebalais/smoke/EditAdmissionTransferAndDischargeTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/EditAdmissionTransferAndDischargeTest.java
@@ -11,23 +11,24 @@ import org.openmrs.module.mirebalais.smoke.pageobjects.LoginPage;
 import org.openmrs.module.mirebalais.smoke.pageobjects.PatientDashboard;
 
 public class EditAdmissionTransferAndDischargeTest extends DbTest {
-	
-	public static final String MALARIA = "B54";
-	
-	@BeforeClass
+
+    private final String malaria = "B54";
+    private final String rubella = "B06.9";
+
+    @BeforeClass
 	public static void prepare() {
 		new LoginPage(driver).logInAsDoctor();
 	}
 	
 	@Test
-	public void changeProviderAndLocationAdmission() throws Exception {
+	public void shouldChangeProviderAndLocationForAdmission() throws Exception {
 		Patient testPatient = PatientDatabaseHandler.insertNewTestPatient();
 		initBasicPageObjects();
 		
 		appDashboard.goToPatientPage(testPatient.getId());
 		patientDashboard.startVisit();
 		
-		patientDashboard.addConsultNoteWithAdmissionToLocation(MALARIA, 3);
+		patientDashboard.addConsultNoteWithAdmissionToLocation(malaria, 3);
 		String provider = patientDashboard.providerForFirstEncounter();
 		String location = patientDashboard.locationForFirstEncounter();
 		
@@ -37,4 +38,25 @@ public class EditAdmissionTransferAndDischargeTest extends DbTest {
 		assertThat(providerAndLocation.getLocation(), not(location));
 		
 	}
+
+    @Test
+     public void shouldChangeProviderAndLocationForTransferWithinHospital() throws Exception {
+        Patient testPatient = PatientDatabaseHandler.insertNewTestPatient();
+        initBasicPageObjects();
+
+        appDashboard.goToPatientPage(testPatient.getId());
+        patientDashboard.startVisit();
+
+        patientDashboard.addConsultNoteWithAdmissionToLocation(malaria, 3);
+        patientDashboard.addConsultNoteWithTransferToLocation(rubella, 3);
+
+        String provider = patientDashboard.providerForFirstEncounter();
+        String location = patientDashboard.locationForFirstEncounter();
+
+        PatientDashboard.ProviderAndLocation providerAndLocation = patientDashboard.editFirstEncounter(3, 4);
+
+        assertThat(providerAndLocation.getProvider(), not(provider));
+        assertThat(providerAndLocation.getLocation(), not(location));
+
+    }
 }
