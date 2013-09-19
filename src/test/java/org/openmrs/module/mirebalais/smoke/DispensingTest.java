259db7419294a97eecdb9b6c576a19ea011258fc
César Henrique Vortmann <cvortmann@gmail.com>
2013-09-19 15:23:38 -0300

    #829 - refactoring test for clarity.

--- src/test/java/org/openmrs/module/mirebalais/smoke/DispensingTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/DispensingTest.java
@@ -1,5 +1,9 @@
 package org.openmrs.module.mirebalais.smoke;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.openmrs.module.mirebalais.smoke.pageobjects.selects.DurationUnit.DAYS;
+
 import org.junit.Test;
 import org.openmrs.module.mirebalais.smoke.dataModel.Patient;
 import org.openmrs.module.mirebalais.smoke.helper.PatientDatabaseHandler;
@@ -7,48 +11,29 @@ import org.openmrs.module.mirebalais.smoke.pageobjects.AppDashboard;
 import org.openmrs.module.mirebalais.smoke.pageobjects.PatientDashboard;
 import org.openmrs.module.mirebalais.smoke.pageobjects.forms.DispenseMedicationForm;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.openmrs.module.mirebalais.smoke.pageobjects.selects.DurationUnit.DAYS;
-
 public class DispensingTest extends DbTest {
-	
-	private final String bronchitis = "J40";
 	
 	private String paracetamol = "Paracetamol (500mg tablet)";
 	
 	@Test
 	public void pharmacistCanDispenseMedicationForAnExistingActiveVisit() throws Exception {
+		AppDashboard appDashboard = new AppDashboard(driver);
+		PatientDashboard patientDashboard = new PatientDashboard(driver);
+		
 		Patient patient = PatientDatabaseHandler.insertNewTestPatient();
 		
 		logInAsClinicalUser();
-		
-		AppDashboard appDashboard = new AppDashboard(driver);
-		
 		appDashboard.goToPatientPage(patient.getId());
-		
-		PatientDashboard patientDashboard = new PatientDashboard(driver);
-		
 		patientDashboard.startVisit();
-		
 		assertThat("Dispense medication.", patientDashboard.canDispenseMedication(), is(false));
-		
-		patientDashboard.addConsultNoteWithAdmissionToLocation(bronchitis, 2);
-		
 		logout();
 		
 		logInAsPharmacistUser();
-		
 		appDashboard.goToPatientPage(patient.getId());
-		
 		assertThat("Dispense medication.", patientDashboard.canDispenseMedication(), is(true));
-		
 		DispenseMedicationForm dispensingForm = patientDashboard.goToDispenseMedicationForm();
-		
 		dispensingForm.fillFirstMedication(paracetamol, "twice per day", "5", "mg", "7", DAYS, "20");
 		dispensingForm.submit();
-		patientDashboard.verifyIfSuccessfulMessageIsDisplayed();
-		
 		patientDashboard.clickFirstEncounterDetails();
 		
 		assertThat("Medication was dispensed.", patientDashboard.countEncountersOfType("Medication dispensed"), is(1));
