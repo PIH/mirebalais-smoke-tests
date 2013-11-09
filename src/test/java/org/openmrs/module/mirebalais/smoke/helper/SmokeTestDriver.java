7c2ccc5a4b42452172aec91619ee8f1fe23b42d7
mogoodrich <mgoodrich@pih.org>
2013-11-09 16:11:00 -0500

    set the implicit wait back to 5 seconds

--- src/test/java/org/openmrs/module/mirebalais/smoke/helper/SmokeTestDriver.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/helper/SmokeTestDriver.java
@@ -15,7 +15,7 @@ public class SmokeTestDriver {
     public SmokeTestDriver() {
         setupChromeDriver();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(20, SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, SECONDS);
         driver.get(new SmokeTestProperties().getWebAppUrl());
     }
 
