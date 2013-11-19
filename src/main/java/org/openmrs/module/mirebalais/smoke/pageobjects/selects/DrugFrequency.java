6643993b06d7b25122d8a076ecdb69fcc331e3de
pamcdm <pmori@pmori.local>
2013-11-19 18:56:28 -0200

    #966 - Add enum for dropdown of drug frequency field at dispensing form

--- src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/selects/DrugFrequency.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/selects/DrugFrequency.java
@@ -1,11 +1,18 @@
 package org.openmrs.module.mirebalais.smoke.pageobjects.selects;
 
-/**
- * Created with IntelliJ IDEA.
- * User: pmori
- * Date: 11/18/13
- * Time: 4:17 PM
- * To change this template use File | Settings | File Templates.
- */
 public enum DrugFrequency {
+    ONCE_A_DAY(1),TWICE_A_DAY(2),THREE_TIMES_A_DAY(3), FOUR_TIMES_A_DAY(4), FIVE_TIMES_A_DAY(5),SIX_TIMES_A_DAY(6),
+    SEVEN_TIMES_A_DAY(7), EIGHT_TIMES_A_DAY(8),NINE_TIMES_A_DAY(9),IN_THE_MORNING(10), AT_NIGHT(11),WHEN_REQUIRED(12),
+    IMMEDIATELY(13), EVERY_TWO_HOURS(14), EVERY_THREE_HOURS(15),EVERY_FOUR_HOURS(16),EVERY_SIX_HOURS(17),
+    EVERY_TWELVE_HOURS(18), OTHER(19);
+
+    private int index;
+
+    DrugFrequency(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
 }
