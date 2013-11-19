3e261a2550e1281ab89b2a4f04beb8a750ee0642
pamcdm <pmori@pmori.local>
2013-11-19 18:57:08 -0200

    #966 - Add enum for dropdown type of prescription field at dispensing form

--- src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/selects/TypeOfPrescrition.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/selects/TypeOfPrescrition.java
@@ -1,11 +1,16 @@
 package org.openmrs.module.mirebalais.smoke.pageobjects.selects;
 
-/**
- * Created with IntelliJ IDEA.
- * User: pmori
- * Date: 11/18/13
- * Time: 5:35 PM
- * To change this template use File | Settings | File Templates.
- */
 public enum TypeOfPrescrition {
+
+    DISCHARGE(1), INPATIENT_HOSPITALIZATION(2);
+
+    private int index;
+
+    TypeOfPrescrition(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
 }
