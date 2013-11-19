a28d6d1284a226c6a482ddaefb72e1f5e9b7e651
pamcdm <pmori@pmori.local>
2013-11-19 18:57:48 -0200

    #966 - Update flow of dispensing tests

--- src/test/java/org/openmrs/module/mirebalais/smoke/DispensingTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/DispensingTest.java
@@ -10,12 +10,15 @@ import org.openmrs.module.mirebalais.smoke.pageobjects.forms.DispenseMedicationF
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.openmrs.module.mirebalais.smoke.pageobjects.selects.DischargeLocation.MIREBALAIS_HOSPITAL;
+import static org.openmrs.module.mirebalais.smoke.pageobjects.selects.DrugFrequency.THREE_TIMES_A_DAY;
 import static org.openmrs.module.mirebalais.smoke.pageobjects.selects.DurationUnit.DAYS;
+import static org.openmrs.module.mirebalais.smoke.pageobjects.selects.TypeOfPrescrition.DISCHARGE;
 
 @Ignore
 public class DispensingTest extends DbTest {
 	
-	private String paracetamol = "Paracetamol (500mg tablet)";
+	private String paracetamol = "Paracetamol, 500mg, tablet";
 	
 	@Test
     @Ignore
@@ -35,21 +38,29 @@ public class DispensingTest extends DbTest {
 		appDashboard.goToPatientPage(patient.getId());
 		assertThat("Dispense medication.", patientDashboard.canDispenseMedication(), is(true));
 		DispenseMedicationForm dispensingForm = patientDashboard.goToDispenseMedicationForm();
-		dispensingForm.fillFirstMedication(paracetamol, "twice per day", "5", "mg", "7", DAYS, "20");
+        dispensingForm.fillDispensingInformation(DISCHARGE, MIREBALAIS_HOSPITAL);
+		dispensingForm.fillFirstMedication(paracetamol, THREE_TIMES_A_DAY , "5", "mg", "7", DAYS, "20");
 		dispensingForm.submit();
+
+
 		patientDashboard.clickFirstEncounterDetails();
 
-        assertThat("Medication was dispensed.", patientDashboard.countEncountersOfType("Medication dispensed"), is(1));
+        assertThat("Medication was dispensed.", patientDashboard.countEncountersOfType("Medikaman Ki Distribye"), is(1));
 
         PatientDashboard.MedicationDispensed medicationDispensed = patientDashboard.firstMedication();
 
-        assertThat("Medication name is right.", medicationDispensed.getName(), is(("Paracetamol (500mg tablet)")));
-        assertThat("Medication frequency is right.", medicationDispensed.getFrequency(), is("twice per day"));
+        assertThat("Medication name is right.", medicationDispensed.getName(), is(("Paracetamol, 500mg, tablet")));
+        assertThat("Medication frequency is right.", medicationDispensed.getFrequency(), is("TID"));
         assertThat("Medication dose is right.", medicationDispensed.getDose(), is("5"));
         assertThat("Medication dose unit is right.", medicationDispensed.getDoseUnit(), is("mg"));
         assertThat("Medication duration is right.", medicationDispensed.getDuration(), is("7"));
         assertThat("Medication duration unit is right.", medicationDispensed.getDurationUnit(), is("Ane"));
         assertThat("Medication amount is right.", medicationDispensed.getAmount(), is("20"));
+
+     //   assertThat("Type of precription is right", medicationDispensed.getTypeOfPrescription(), is("Egzeyat"));
+     //   assertThat("Discharge location is right",  medicationDispensed.getDischargeLocation();, is("Mirebalais Hospital"));
+
+
 
     }
 	
