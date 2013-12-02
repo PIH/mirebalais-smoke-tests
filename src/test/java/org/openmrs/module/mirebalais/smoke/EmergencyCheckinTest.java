2b4b689f639c809dd043ad450943587ff73969aa
mogoodrich <mgoodrich@pih.org>
2013-12-02 12:35:36 -0500

    modified emergency check-in test so that it no longer utilizes the quick stats reports

--- src/test/java/org/openmrs/module/mirebalais/smoke/EmergencyCheckinTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/EmergencyCheckinTest.java
@@ -2,38 +2,27 @@ package org.openmrs.module.mirebalais.smoke;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.stringContainsInOrder;
 import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.openmrs.module.mirebalais.smoke.helper.PatientDatabaseHandler;
+import org.openmrs.module.mirebalais.smoke.pageobjects.EmergencyCheckin;
+import org.openqa.selenium.JavascriptExecutor;
 
 import java.math.BigInteger;
 import java.util.Arrays;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.openmrs.module.mirebalais.smoke.dataModel.BasicReportData;
-import org.openmrs.module.mirebalais.smoke.dataModel.Patient;
-import org.openmrs.module.mirebalais.smoke.helper.PatientDatabaseHandler;
-import org.openmrs.module.mirebalais.smoke.pageobjects.BasicReportPage;
-import org.openmrs.module.mirebalais.smoke.pageobjects.EmergencyCheckin;
-import org.openmrs.module.mirebalais.smoke.pageobjects.ReportsHomePage;
-import org.openqa.selenium.JavascriptExecutor;
-
 public class EmergencyCheckinTest extends DbTest {
 	
 	@Test
-	public void checkinOnEmergencyShouldCountOnReports() throws Exception {
-		initBasicPageObjects();
+	public void checkinOnEmergencyShouldCheckInUnidentifiedPatient() throws Exception {
+
+        initBasicPageObjects();
         EmergencyCheckin emergencyCheckinPO = new EmergencyCheckin(driver);
-        ReportsHomePage reportsHomePage = new ReportsHomePage(driver);
-        BasicReportPage basicReport = new BasicReportPage(driver);
 
         login();
-		
-		appDashboard.openReportApp();
-		reportsHomePage.openBasicStatisticsReport();
-		BasicReportData brd1 = basicReport.getData();
-		
+
 		appDashboard.openStartHospitalVisitApp();
 		emergencyCheckinPO.checkinMaleUnindentifiedPatient();
 		
@@ -42,13 +31,6 @@ public class EmergencyCheckinTest extends DbTest {
 		assertThat(patientRegistrationDashboard.getName(), stringContainsInOrder(Arrays.asList("UNKNOWN", "UNKNOWN")));
 		
 		populateTestPatientForTearDown();
-		
-		appDashboard.openReportApp();
-		reportsHomePage.openBasicStatisticsReport();
-		BasicReportData brd2 = basicReport.getData();
-		
-		assertThat(brd2.getOpenVisits(), greaterThan(brd1.getOpenVisits()));
-		assertThat(brd2.getRegistrationToday(), greaterThan(brd1.getRegistrationToday()));
 	}
 	
 	private void populateTestPatientForTearDown() throws Exception {
