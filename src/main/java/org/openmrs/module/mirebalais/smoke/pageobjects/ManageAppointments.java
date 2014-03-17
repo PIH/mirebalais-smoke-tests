289b9b00e53fb4249e3fe730bba1da82f52abc52
Luiza Souza <lsouza@thoughtworks.com>
2014-03-17 15:48:40 -0300

    UHM-918 - Add wait time to load the page

--- src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/ManageAppointments.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/ManageAppointments.java
@@ -1,30 +1,31 @@
 package org.openmrs.module.mirebalais.smoke.pageobjects;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class ManageAppointments {
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
-    private WebDriver driver;
-
+public class ManageAppointments extends AbstractPageObject {
     public ManageAppointments(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
-    public void enterPatientIdentifier(String patientID) {
+    public void enterPatientIdentifier(String patientID) throws InterruptedException {
         WebElement searchField = driver.findElement(By.id("patient-search"));
         searchField.sendKeys(patientID);
-        searchField.sendKeys(Keys.ARROW_DOWN);
-        searchField.sendKeys(Keys.RETURN);
+        wait15seconds.until(presenceOfElementLocated(By.tagName("tbody")));
+        WebElement tbody = driver.findElement(By.tagName("tbody"));
+        tbody.findElement(By.tagName("tr")).click();
     }
 
     public boolean isSelectServiceTypeDefined() {
+        wait5seconds.until(presenceOfElementLocated(By.id("selectAppointmentType")));
         return (driver.findElement(By.id("selectAppointmentType")) != null) ? true : false;
     }
 
     public boolean isDateRangePickerDefined() {
+        wait5seconds.until(presenceOfElementLocated(By.tagName("daterangepicker")));
         return (driver.findElement(By.tagName("daterangepicker")) != null) ? true : false;
     }
 
