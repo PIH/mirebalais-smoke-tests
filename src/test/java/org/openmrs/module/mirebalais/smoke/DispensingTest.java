ec86409e7925af8790ee1f17d2646950d47450c6
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-03-21 19:07:44 -0700

    Fix smoke test broken as part of UHM-1038 (Dropdown on dosage units in Dispensing form)

--- src/test/java/org/openmrs/module/mirebalais/smoke/DispensingTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/DispensingTest.java
@@ -18,8 +18,9 @@ public class DispensingTest extends DbTest {
 	
 	private String paracetamol = "Paracetamol, 500mg, tablet";
     private String prescriptionLocation = "Sal Fanm";
-	
-	@Test
+    private String doseUnit = "Milligramme (mg)";
+
+    @Test
 	public void pharmacistCanDispenseMedicationForAnExistingActiveVisit() throws Exception {
 		AppDashboard appDashboard = new AppDashboard(driver);
 		PatientDashboard patientDashboard = new PatientDashboard(driver);
@@ -37,7 +38,7 @@ public class DispensingTest extends DbTest {
 		assertThat("Dispense medication.", patientDashboard.canDispenseMedication(), is(true));
 		DispenseMedicationForm dispensingForm = patientDashboard.goToDispenseMedicationForm();
         dispensingForm.fillDispensingInformation(DISCHARGE, prescriptionLocation);
-		dispensingForm.fillFirstMedication(paracetamol, THREE_TIMES_A_DAY , "5", "Milligramme (mg)", "7", DAYS, "20");
+        dispensingForm.fillFirstMedication(paracetamol, THREE_TIMES_A_DAY , "5", doseUnit, "7", DAYS, "20");
 		dispensingForm.submit();
 
 
@@ -50,7 +51,7 @@ public class DispensingTest extends DbTest {
         assertThat("Medication name is right.", medicationDispensed.getName(), is(("Paracetamol, 500mg, tablet")));
         assertThat("Medication frequency is right.", medicationDispensed.getFrequency(), is("TID"));
         assertThat("Medication dose is right.", medicationDispensed.getDose(), is("5"));
-        assertThat("Medication dose unit is right.", medicationDispensed.getDoseUnit(), is("mg"));
+        assertThat("Medication dose unit is right.", medicationDispensed.getDoseUnit(), is(doseUnit));
         assertThat("Medication duration is right.", medicationDispensed.getDuration(), is("7"));
         assertThat("Medication duration unit is right.", medicationDispensed.getDurationUnit(), is("Jou"));
         assertThat("Medication amount is right.", medicationDispensed.getAmount(), is("20"));
