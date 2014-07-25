d41f31ba5de7056e979fa387c95c60fd6a817082
mogoodrich <mgoodrich@pih.org>
2014-07-25 17:09:53 -0400

    UHM-1489: Create Wristbands at Emergency Check-In updated name of check-in app

--- src/main/java/org/openmrs/module/mirebalais/smoke/helper/SmokeTestProperties.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/helper/SmokeTestProperties.java
@@ -5,19 +5,26 @@ public class SmokeTestProperties {
     public static final int IMPLICIT_WAIT_TIME = 10;
 
     public String getWebAppUrl() {
-        return envOrDefault("WEBAPP_URL", "http://localhost:8080/openmrs");
+        //return envOrDefault("WEBAPP_URL", "http://localhost:8080/openmrs_1.9");
+        return envOrDefault("WEBAPP_URL", "http://bamboo.pih-emr.org:8080/mirebalais");
     }
 
     public String getDatabaseUrl() {
-        return envOrDefault("DATABASE_URL", "jdbc:mysql://localhost:3306/openmrs");
+        //return envOrDefault("DATABASE_URL", "jdbc:mysql://localhost:3306/openmrs_1_9");
+        return envOrDefault("DATABASE_URL", "jdbc:mysql://bamboo.pih-emr.org:3306/openmrs");
     }
 
     public String getDatabaseUsername() {
-        return envOrDefault("DATABASE_USERNAME", "openmrs");
+        //return envOrDefault("DATABASE_USERNAME", "openmrs_1_9_user");
+        return envOrDefault("DATABASE_USERNAME", "openmrs_editor");
     }
 
     public String getDatabasePassword() {
-        return envOrDefault("DATABASE_PASSWORD", "openmrs");
+        //return envOrDefault("DATABASE_PASSWORD", "c65Ln9e^.di1");
+        return envOrDefault("DATABASE_PASSWORD", "dr0pper");
+
+
+
     }
 
     public String getDatabaseDriverClass() {
