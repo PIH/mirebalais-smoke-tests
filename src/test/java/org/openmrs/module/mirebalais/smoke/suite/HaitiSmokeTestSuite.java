b8dceb1df6431353881a3aac024e17c55554c111
mogoodrich <mgoodrich@pih.org>
2015-05-26 18:38:45 -0400

    fixed name of patient registration test referenced in haiti smoke test suite

--- src/test/java/org/openmrs/module/mirebalais/smoke/suite/HaitiSmokeTestSuite.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/suite/HaitiSmokeTestSuite.java
@@ -5,12 +5,12 @@ import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.openmrs.module.mirebalais.smoke.BasicMirebalaisSmokeTest;
-import org.openmrs.module.mirebalais.smoke.PatientRegistrationFlowTest;
+import org.openmrs.module.mirebalais.smoke.PatientRegistrationHaitiFlowTest;
 import org.openmrs.module.mirebalais.smoke.helper.SmokeTestDriver;
 import org.openqa.selenium.WebDriver;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({PatientRegistrationFlowTest.class})
+@Suite.SuiteClasses({PatientRegistrationHaitiFlowTest.class})
 public class HaitiSmokeTestSuite {
 
     private static WebDriver driver;
