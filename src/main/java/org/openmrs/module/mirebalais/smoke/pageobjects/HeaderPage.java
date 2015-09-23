7059ca19b9d102d16599b2cdc3a0e9b267e34613
mogoodrich <mgoodrich@pih.org>
2015-09-23 16:13:50 -0400

    fix to actually click on logout button!

--- src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/HeaderPage.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/HeaderPage.java
@@ -27,7 +27,7 @@ public class HeaderPage extends AbstractPageObject {
 
 	public void logOut() {
         wait5seconds.until(elementToBeClickable((By.className("logout")))); // sometimes tests fail because a toast message is blocking the logout button, this should wait until the toast message closes
-		driver.findElement(By.className("logout"));
+		driver.findElement(By.className("logout")).click();
 	}
 
     public void home() {
