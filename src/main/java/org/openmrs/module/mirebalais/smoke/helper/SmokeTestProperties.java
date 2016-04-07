6c88714ff5671074f24f4dc7259478486c928d08
mogoodrich <mgoodrich@pih.org>
2016-04-07 11:07:49 -0400

    cache smoke test properties

--- src/main/java/org/openmrs/module/mirebalais/smoke/helper/SmokeTestProperties.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/helper/SmokeTestProperties.java
@@ -4,20 +4,44 @@ public class SmokeTestProperties {
 
     public static final int IMPLICIT_WAIT_TIME = 10;
 
+    private String webAppUrl = null;
+
+    private String databaseUrl = null;
+
+    private String databaseUsername = null;
+
+    private String databasePassword = null;
+
     public String getWebAppUrl() {
-        return envOrDefault("WEBAPP_URL", "http://localhost:8080/openmrs");
+        if (webAppUrl == null) {
+            webAppUrl = envOrDefault("WEBAPP_URL", "http://localhost:8080/openmrs");
+        }
+
+        return webAppUrl;
     }
 
     public String getDatabaseUrl() {
-        return envOrDefault("DATABASE_URL", "jdbc:mysql://localhost:3306/openmrs");
+        if (databaseUrl == null) {
+            databaseUrl = envOrDefault("DATABASE_URL", "jdbc:mysql://localhost:3306/openmrs");
+        }
+
+        return databaseUrl;
     }
 
     public String getDatabaseUsername() {
-        return envOrDefault("DATABASE_USERNAME", "openmrs");
+        if (databaseUsername == null) {
+            databaseUsername = envOrDefault("DATABASE_USERNAME", "openmrs");
+        }
+
+        return databaseUsername;
     }
 
     public String getDatabasePassword() {
-        return envOrDefault("DATABASE_PASSWORD", "openmrs");
+        if (databasePassword == null) {
+            databasePassword = envOrDefault("DATABASE_PASSWORD", "openmrs");
+        }
+
+        return databasePassword;
     }
 
     public String getDatabaseDriverClass() {
