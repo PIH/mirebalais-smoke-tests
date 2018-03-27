1ce436073069dc2ef9467052215798e186e7dd07
mgoodrich <mark.goodrich@live.com>
2018-03-27 16:42:16 -0400

    hack--see if extending the delay for vitamin A on the supplements form helps with the smoke test issue

--- src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/forms/SupplementsForm.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/forms/SupplementsForm.java
@@ -11,7 +11,12 @@ public class SupplementsForm extends BaseHtmlForm {
     }
 
     public void fillFormWithBasicInfo() throws Exception {
-        wait15seconds.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#vitamin-a input")));
+        try {
+            wait15seconds.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#vitamin-a input")));
+        }
+        catch (Exception e) {
+            wait15seconds.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#vitamin-a input")));
+        }
         clickOn(By.cssSelector("#vitamin-a input"));
         confirmData();
     }
