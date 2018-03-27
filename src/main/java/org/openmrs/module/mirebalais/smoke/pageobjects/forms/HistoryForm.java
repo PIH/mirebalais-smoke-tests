61f71ec05e196a6731c3b7b87ced1f080d30fe31
mgoodrich <mark.goodrich@live.com>
2018-03-27 16:13:23 -0400

    change History form to delay for presence of textarea to see if it makes tests more stable

--- src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/forms/HistoryForm.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/forms/HistoryForm.java
@@ -3,6 +3,7 @@ package org.openmrs.module.mirebalais.smoke.pageobjects.forms;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HistoryForm extends BaseHtmlForm {
 
@@ -11,6 +12,7 @@ public class HistoryForm extends BaseHtmlForm {
     }
 
     public void fillFormWithBasicInfo(String historyText) {
+        wait5seconds.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#presenting-history textarea")));
         WebElement history = driver.findElement(By.cssSelector("#presenting-history textarea"));
         history.sendKeys(historyText);
         confirmData();
