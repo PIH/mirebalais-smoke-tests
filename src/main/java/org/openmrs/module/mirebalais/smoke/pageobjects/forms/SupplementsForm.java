0046c184831937990cb9e20c2d92eb8f1dfcdf1f
mgoodrich <mark.goodrich@live.com>
2018-03-29 13:18:59 -0400

    revert hack to supplements form

--- src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/forms/SupplementsForm.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/forms/SupplementsForm.java
@@ -11,12 +11,7 @@ public class SupplementsForm extends BaseHtmlForm {
     }
 
     public void fillFormWithBasicInfo() throws Exception {
-        try {
-            wait15seconds.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#vitamin-a input")));
-        }
-        catch (Exception e) {
-            wait15seconds.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#vitamin-a input")));
-        }
+        wait15seconds.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#vitamin-a input")));
         clickOn(By.cssSelector("#vitamin-a input"));
         confirmData();
     }
