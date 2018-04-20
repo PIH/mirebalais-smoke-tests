6ea84e59bbb0e7d8be7a27cf6362c27f35c6ec6c
Dominic Surrao <dsurrao@hotmail.com>
2018-04-20 16:40:45 -0400

    EDTriage tests

---
+++ src/test/java/org/openmrs/module/mirebalais/smoke/EDTriageTest.java
@@ -0,0 +1,44 @@
+package org.openmrs.module.mirebalais.smoke;
+
+import org.junit.Test;
+import org.openmrs.module.mirebalais.smoke.dataModel.Patient;
+import org.openmrs.module.mirebalais.smoke.helper.PatientDatabaseHandler;
+import org.openmrs.module.mirebalais.smoke.pageobjects.EDTriageEditPatientPage;
+import org.openmrs.module.mirebalais.smoke.pageobjects.EDTriageQueuePage;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class EDTriageTest extends DbTest {
+
+    @Test
+    public void addPatientToQueue() throws Exception {
+        String chiefComplaint = "bruises";
+
+        // create patient
+        Patient testPatient = PatientDatabaseHandler.insertAdultTestPatient();
+
+        // login to "Ijans" location
+        logInAsAdmin("Ijans");
+
+        // open ED Triage app
+        initBasicPageObjects();
+        appDashboard.openEDTriageApp();
+
+        // search for patient
+        appDashboard.findPatientByIdentifier(testPatient.getIdentifier());
+
+        // enter chief complaint for patient and save
+        EDTriageEditPatientPage editPage = new EDTriageEditPatientPage(driver);
+        editPage.enterChiefComplaintAndSave(chiefComplaint);
+
+        // open ED Triage Queue app
+        appDashboard.openEDTriageQueueApp();
+
+        // confirm that patient is in the queue
+        // TODO: check complaint also!
+        EDTriageQueuePage queuePage = new EDTriageQueuePage(driver);
+        boolean patientExists = queuePage.lookForPatient(testPatient);
+        assertTrue(patientExists);
+    }
+}
