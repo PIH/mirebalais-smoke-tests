6c7c94fe294554fa62ff92fbdb98aacfe8411f56
mgoodrich <mark.goodrich@live.com>
2019-11-19 09:45:31 -0500

    make webapp name customizable

--- src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/HeaderPage.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/pageobjects/HeaderPage.java
@@ -25,7 +25,7 @@ public class HeaderPage extends AbstractPageObject {
 
 	public void logOut() {
         // hack, just go to the logout url since the we are having so much trouble with the toast message overlapping the logout button
-        driver.get(properties.getWebAppUrl() + "/appui/header/logout.action?successUrl=openmrs");
+        driver.get(properties.getWebAppUrl() + "/appui/header/logout.action?successUrl=" + properties.getWebAppName());
     }
 
     public void home() {
