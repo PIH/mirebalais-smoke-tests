00e0ec8cb84c0e0b89ef862fe5c56fc0f675f3f5
Brandon Istenes <bistenes@gmail.com>
2020-05-04 08:18:56 -0700

    Upgrade to Selenium 3, Java 8, Chromedriver 81 (#8)

--- src/test/java/org/openmrs/module/mirebalais/smoke/HeaderTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/HeaderTest.java
@@ -2,19 +2,21 @@ package org.openmrs.module.mirebalais.smoke;
 
 import org.junit.Test;
 import org.openmrs.module.mirebalais.smoke.pageobjects.HeaderPage;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElement;
 
 public class HeaderTest extends BasicMirebalaisSmokeTest {
-	
-	private WebDriverWait wait5Second = new WebDriverWait(driver, 5);
+
+	private final WebDriverWait wait5Seconds = new WebDriverWait(driver, 5);
 
 	@Test
 	public void shouldChangeLocation() throws Exception {
 		login();
 		HeaderPage headerPage = new HeaderPage(driver);
+		WebElement header = driver.findElement(headerPage.location());
         String locationName = headerPage.changeLocationTo(4);
-		wait5Second.until(textToBePresentInElement(headerPage.location(), locationName));
+		wait5Seconds.until(textToBePresentInElement(header, locationName));
 	}
 }
