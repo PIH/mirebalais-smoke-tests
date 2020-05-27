e07e2764b957d57f5a1f8c86f586997abfd91614
mgoodrich <mark.goodrich@live.com>
2020-05-27 14:12:28 -0400

    archives room app  no longer appears unless at Archives location

--- src/test/java/org/openmrs/module/mirebalais/smoke/UserAdminTest.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/UserAdminTest.java
@@ -18,42 +18,42 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class UserAdminTest extends DbTest {
-	
+
 	private static final String DEFAULT_PASSWORD = "Teste123";
-	
+
 	private UserAdmin userAdmin;
-	
+
 	private HeaderPage header;
-	
+
 	private MyAccountApp myAccountApp;
-	
+
 	private String username;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		initBasicPageObjects();
 		userAdmin = new UserAdmin(driver);
 		header = new HeaderPage(driver);
 		myAccountApp = new MyAccountApp(driver);
-		
+
 		username = createUser();
-		
+
 		login();
 		appDashboard.openSysAdminApp();
 	}
-	
+
 	@Test
 	public void createUserWithPhysicianRoleAndUserChangesOwnPassword() throws Exception {
 
         userAdmin.createPhysicianAccount(NameGenerator.getUserFirstName(), NameGenerator.getUserLastName(), username,
                 DEFAULT_PASSWORD);
 		logOutAndLogInWithNewUser(username);
-		
+
 		appDashboard.openMyAccountApp();
 		myAccountApp.openChangePassword();
 		myAccountApp.changePassword(DEFAULT_PASSWORD, DEFAULT_PASSWORD);
 		logOutAndLogInWithNewUser(username);
-		
+
 		assertThat(appDashboard.isActiveVisitsAppPresented(), is(true));
 
         turnOffImplicitWaits(); // once we've found one element, assume that all are present
@@ -70,7 +70,7 @@ public class UserAdminTest extends DbTest {
 		assertThat(appDashboard.isLegacyAppPresented(), is(false));
         turnOnImplicitWait();
 	}
-	
+
 	@Test
 	public void createUserWithScheduleManagerRole() throws Exception {
 		userAdmin.createScheduleManagerAccount(NameGenerator.getUserFirstName(), NameGenerator.getUserLastName(), username,
@@ -94,7 +94,7 @@ public class UserAdminTest extends DbTest {
         assertThat(appDashboard.isLegacyAppPresented(), is(false));
         turnOnImplicitWait();
 	}
-	
+
 	@Test
 	public void createUserWithArchivistClerkRole() throws Exception {
 		userAdmin.createArchivistClerkAccount(NameGenerator.getUserFirstName(), NameGenerator.getUserLastName(), username,
@@ -102,9 +102,9 @@ public class UserAdminTest extends DbTest {
 
 		logOutAndLogInWithNewUser(username);
 
-        assertThat(appDashboard.isArchivesRoomAppPresented(), is(true));
-        turnOffImplicitWaits(); // once we've found one element, assume that all are present
+        //assertThat(appDashboard.isArchivesRoomAppPresented(), is(true));
         assertThat(appDashboard.isPatientRegistrationAppPresented(), is(true));
+        turnOffImplicitWaits(); // once we've found one element, assume that all are present
         assertThat(appDashboard.isStartClinicVisitAppPresented(), is(true));
 		assertThat(appDashboard.isReportsAppPresented(), is(true));
 
@@ -117,7 +117,7 @@ public class UserAdminTest extends DbTest {
         assertThat(appDashboard.isInpatientsAppPresented(), is(false));
         turnOnImplicitWait();
 	}
-	
+
 	@Test
 	public void createUserWithSysAdminRoleWithEnglishAsDesiredLanguage() throws Exception {
 		userAdmin.createSysAdminAccount(NameGenerator.getUserFirstName(), NameGenerator.getUserLastName(), username,
@@ -131,35 +131,35 @@ public class UserAdminTest extends DbTest {
         assertThat(appDashboard.isSystemAdministrationAppPresented(), is(true));
         assertThat(appDashboard.isReportsAppPresented(), is(true));
         assertThat(appDashboard.isInpatientsAppPresented(), is(true));
-        assertThat(appDashboard.isArchivesRoomAppPresented(), is(true));
+        //assertThat(appDashboard.isArchivesRoomAppPresented(), is(true));
         assertThat(appDashboard.isPatientRegistrationAppPresented(), is(true));
         assertThat(appDashboard.isStartClinicVisitAppPresented(), is(true));
         assertThat(appDashboard.isUhmCaptureVitalsAppPresented(), is(true));
 
         assertThat(appDashboard.isLegacyAppPresented(), is(false));
         turnOnImplicitWait();
-		
+
 		appDashboard.openSysAdminApp();
         // confirm localized in English
 		String text = driver.findElement(By.id(replaceChars(CustomAppLoaderConstants.Apps.MANAGE_ACCOUNTS, ".", "-") + SysAdminPage.SYSTEM_ADMINISTRATION_APP_LINK_SUFFIX)).getText();
 		assertFalse(text.contains("Jere Kont"));
 		assertTrue(text.contains("Manage Accounts"));
-		
+
 	}
-	
+
 	@After
 	public void tearDown() {
 		header.logOut();
 	}
-	
+
 	private String createUser() {
 		return new String("user" + System.currentTimeMillis());
 	}
-	
+
 	private void logOutAndLogInWithNewUser(String username) throws InterruptedException {
 		Thread.sleep(5000);
         header.logOut();
 		loginPage.logIn(username, DEFAULT_PASSWORD);
 	}
-	
+
 }
