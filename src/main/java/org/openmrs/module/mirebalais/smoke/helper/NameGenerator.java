d7318abcbe0b25acb7f24738f96741c9eaaf28b8
mgoodrich <mark.goodrich@live.com>
2020-09-28 09:59:53 -0400

    Remove apostrophe (HTML character) from service type name

--- src/main/java/org/openmrs/module/mirebalais/smoke/helper/NameGenerator.java
+++ src/main/java/org/openmrs/module/mirebalais/smoke/helper/NameGenerator.java
@@ -5,20 +5,20 @@ import java.util.Random;
 
 public class NameGenerator {
 
-	private static final String[] PATIENT_FIRST_NAMES = { "Alexandre", "Achint", 
+	private static final String[] PATIENT_FIRST_NAMES = { "Alexandre", "Achint",
 			"Brittany", "Burke", "Cesar", "Cosmin", "Daniel", "Darius", "David", "Ellen",
 			"Émerson", "Evan", "Fernando", "Gabou", "Glauber", "Hamish", "Louise", "Mário",
 			"Mark", "Natália", "Neil", "Neissi", "Nelice", "Rafal", "Renee", "Wyclif" };
-	
+
 	private static final String[] PATIENT_LAST_NAMES = { "Barbosa", "Sethi", "Eddy",
 			"Mamlin", "Vortmann", "Ioan", "Kayiwa", "Jazayeri", "Walton", "Ball", "Hernandez",
 			"Waters", "Freire", "Mandy", "Ramos", "Fraser", "Sécordel", "Areias", "Goodrich",
 			"Arsand", "Craven", "Lima", "Heck", "Korytkowski", "Orser", "Luyima" };
-	
+
 	private static final String[] USER_FIRST_NAMES = { "Ororo", "Wally",
 			"Clint", "Charles", "Clark", "Bruce", "Peter", "Diana", "Tony",
 			"Steve", "Reed", "James", "Hal", "Matt", "Dick", "Jean", "Barbara" };
-	
+
 	private static final String[] USER_LAST_NAMES = { "Kent", "Wayne",
 			"Parker", "Banner", "Prince", "Stark", "Rogers", "Richards",
 			"Howlett", "Jordan", "Murdock", "Grayson", "Gray", "Gordon",
@@ -28,7 +28,7 @@ public class NameGenerator {
             "Radiology, new patient", "Orthopedics, new patient", "Orthopedics, follow up",
             "Mental health, follow up", "Mental health, new patient", "Adult chronic diseases, follow up",
             "Adult chronic diseases, new patient", "Oncology, new patient", "Oncology, follow up", "Dental, new patient",
-            "Dental, follow up", "Women's health family, follow up", "Women's health family, new patient"};
+            "Dental, follow up", "Womens health family, follow up", "Womens health family, new patient"};
 
 	public static String getPatientFirstName() {
 		return PATIENT_FIRST_NAMES[(int) (Math.random() * PATIENT_FIRST_NAMES.length)];
@@ -41,7 +41,7 @@ public class NameGenerator {
 	public static String getPatientName() {
 		return new StringBuilder(NameGenerator.getPatientFirstName() + " " + NameGenerator.getPatientLastName()).toString();
 	}
-	
+
 	public static String getUserFirstName() {
 		return USER_FIRST_NAMES[(int) (Math.random() * USER_FIRST_NAMES.length)];
 	}
@@ -49,7 +49,7 @@ public class NameGenerator {
 	public static String getUserLastName() {
 		return USER_LAST_NAMES[(int) (Math.random() * USER_LAST_NAMES.length)];
 	}
-	
+
 	public static String getUserName() {
 		return new StringBuilder(NameGenerator.getUserFirstName() + " " + NameGenerator.getUserLastName()).toString();
 	}
