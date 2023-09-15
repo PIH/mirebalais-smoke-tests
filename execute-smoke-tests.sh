5133aec60ed12b1af5cdda1974e3074c56aef82e
Mark Goodrich <mgoodrich@pih.org>
2023-09-15 13:44:07 -0400

    add debug flag for smoke tests

--- execute-smoke-tests.sh
+++ execute-smoke-tests.sh
@@ -4,4 +4,4 @@ SUITE=$1
 
 export DISPLAY=:20
 Xvfb :20 -screen 0 1366x768x16 &
-mvn clean verify -U -P${SUITE} -Dsurefire.skipAfterFailureCount=1
+mvn clean verify -U -X -P${SUITE} -Dsurefire.skipAfterFailureCount=1
