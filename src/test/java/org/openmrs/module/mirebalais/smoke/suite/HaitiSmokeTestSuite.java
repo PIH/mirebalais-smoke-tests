2c6e4a97104a6dae367f849b6b97df7a62618a08
Mark Goodrich <mgoodrich@pih.org>
2023-12-22 17:45:01 -0500

    Change HaitiSmokeTestSuite to not run VisitNoteTests since the flow is slightly different in the Thomonde config (which is what ci.pih-emr.org is currently set to mirror)

--- src/test/java/org/openmrs/module/mirebalais/smoke/suite/HaitiSmokeTestSuite.java
+++ src/test/java/org/openmrs/module/mirebalais/smoke/suite/HaitiSmokeTestSuite.java
@@ -16,9 +16,11 @@ import org.openqa.selenium.WebDriver;
 @Suite.SuiteClasses({
         CaptureVitalsTest.class,
         PatientRegistrationHaitiFlowTest.class,
+        // this has been disable since we don't currently have them turned on on HSN
       //  CaptureVitalsHSNTest.class,
        // CheckInHSNTest.class,
        // PatientRegistrationHSNFlowTest.class,
+        // this has been disabled since the Thomonde Visit Note vitals flow is slightly different from the Mirebalais one
         VisitNoteTest.class
                     })
 public class HaitiSmokeTestSuite {
