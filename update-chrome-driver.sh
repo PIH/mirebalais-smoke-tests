d56f0a62fe12aabc457e6903ec81099e14b7312b
Mark Goodrich <mgoodrich@pih.org>
2024-02-22 12:58:27 -0500

    update update-chrome-driver.sh

--- update-chrome-driver.sh
+++ update-chrome-driver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -fR src/test/resources/chromedriver/linux/*
-wget -O /tmp/chromedriver.zip https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$(wget -qO- https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_STABLE)/linux64/chromedriver-linux64.zip
+wget -O /tmp/chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/$(wget -qO- https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_STABLE)/linux64/chromedriver-linux64.zip
 unzip -j /tmp/chromedriver.zip 'chromedriver-linux64/chromedriver' -d src/test/resources/chromedriver/linux
